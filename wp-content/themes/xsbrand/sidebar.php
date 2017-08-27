25f69ecef35f1f0c1e71b076873a423e48c6465a
Xu Shengping <xspkevin@163.com>
2017-08-28 00:34:34 +0800

    201708280024

--- wp-content/themes/xsbrand/sidebar.php
+++ wp-content/themes/xsbrand/sidebar.php
@@ -1,6 +1,27 @@
 
-<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+<?php //if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		<?php else: ?>	 
-	<?php endif; ?>
+		<?php //dynamic_sidebar( 'sidebar-1' ); ?>
+		<?php //else: ?>
+	<?php //endif; ?>
+
+
+<aside id="recent-posts-2" class="widget widget_recent_entries">
+	<h3 class="widget-title">联系我们</h3>
+	<div class="textwidget">
+		<ul>
+			<li>设计总负责（刘工）：<br />TEL：18898363613<br />Q Q：10966183</li>
+			<li>结构负责（田工）：<br />TEL：15989318545<br />Q Q：286374061</li>
+			<li>设计、施工讨论群：<br />QQ群：434568262</li>
+		</ul>
+	</div>
+</aside>
+
+<aside id="black-studio-tinymce-2" class="widget widget_black_studio_tinymce">
+	<h3 class="widget-title">微信咨询</h3>
+	<div class="textwidget">
+		<p>
+			<img src="<?php the_field('wechat','option') ?>" alt="扫一扫">
+		</p>
+	</div>
+</aside>
--- wp-content/themes/xsbrand/single.php
+++ wp-content/themes/xsbrand/single.php
@@ -8,10 +8,12 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'content/content-single'); ?>
 <?php endwhile;  endif;?>
+	<!--
 <nav id="nav-single" class="clearfix">
-		<div class="nav-previous"><?php if (get_previous_post()) { previous_post_link('下一篇: %link');} else {echo "没有了，已经是最后文章";} ?></div>
-		<div class="nav-next"><?php if (get_next_post()) { next_post_link('上一篇: %link');} else {echo "没有了，已经是最新文章";} ?></div>
+		<div class="nav-previous"><?php //if (get_previous_post()) { previous_post_link('下一篇: %link');} else {echo "没有了，已经是最后文章";} ?></div>
+		<div class="nav-next"><?php //if (get_next_post()) { next_post_link('上一篇: %link');} else {echo "没有了，已经是最新文章";} ?></div>
 	</nav>
+	-->
 </div>
 <div class="col-md-3 col-sm-3 hidden-xs sidebar">
 <?php get_sidebar();?>
